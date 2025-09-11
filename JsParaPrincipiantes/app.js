5b2d738f8091208bc65ae6192d02c65b978b7531
IsraelEsauRicoRamirez <124049153@upq.edu.mx>
2025-09-10 18:54:03 -0600

    Curso avance

---
+++ JsParaPrincipiantes/app.js
@@ -0,0 +1,34 @@
+// Tipos de datos 
+"Hello World"// Cadena de caracteres (String)
+'Hello World'// String
+
+
+console.log('Esto e una cadena');
+10000 // Operar dentro de las aplicaciones
+1000.1// numbers
+-2.3
+
+// boolean
+true
+false
+
+// array (arreglo)
+['joe' ,'ryan' ,'martha']// Lista de strings
+[1, 2, 3]
+[true, false, true, false]
+
+
+// Object (objeto)
+'ryan'
+70.4
+14
+true
+
+{
+   "username": 'ryan', // clave y valor
+   "score": 70.4,// clave y valor
+   "hours": 14,// clave y valor
+   "proffesional": true// clave y valor
+}
+
+
