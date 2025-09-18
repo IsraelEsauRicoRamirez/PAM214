e255249b9201b62cfa1f7a2bcd8260960aee051e
IsraelEsauRicoRamirez <124049153@upq.edu.mx>
2025-09-17 19:00:10 -0600

    Cuarto commit
    Ejercicio (c) terminado.

--- Practica1/Practica1.js
+++ Practica1/Practica1.js
@@ -6,17 +6,26 @@ let nombre = "Armando";
 const edad = 25;
 
 nombre = "Ana Maria";
+nombre = "Israel"
 
 let saludo = "Hola, " + nombre +  ". Tienes " + edad + " años.";
  console.log(saludo)
  */
 
-
+/*
  // Ejercicio (b)
-
 
  let cuadrado = (num)=> num * num
  
  console.log(cuadrado(4))
   console.log(cuadrado(9))
    console.log(cuadrado(5))
+
+*/
+// Ejercicio (c)
+
+let saludo2 = (nombre, edad)=> "Hola, me llamo " + nombre + " y tengo " + edad + " años."
+console.log(saludo2("Luis ",25))
+
+
+
