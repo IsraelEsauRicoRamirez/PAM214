883e5d35c80c219c3535e4f062926289b6d71766
IsraelEsauRicoRamirez <124049153@upq.edu.mx>
2025-10-30 20:02:42 -0600

    Commit:  ScrollView & ActivityIndicator Completada
    Esta practica que se realizo el día de hoy en la sesión virtual se termino. En este caso Yo presente ActivityIndicator.

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,14 +1,73 @@
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Button, ActivityIndicator } from 'react-native'
+
+
+
 
 
 export default function ActivityIndicatorScreen()  {
+
+  const [cargando, setCargando]= useState(false);
+
+
+  const iniciarCarga = () => {
+    setCargando(true);
+    setTimeout(() => setCargando(false), 3000);
+  };
+
   
     return (
-      <View>
-        <Text>Proximamente...</Text>
+
+
+      <View style={styles.container}>
+        <Text style={styles.texto}>
+          'Presiona aqui para iniciar carga'
+        </Text>
+
+        <ActivityIndicator
+
+        style={styles.indicador}
+        animating={cargando}
+        size="large"
+        color="blue"
+        hidesWhenStopped={true}
+      
+        />
+        <View style={styles.botonContainer}>
+
+          <Button color="red" title='Iniciar Carga' onPress={iniciarCarga}/>
+
+
+
+
+
+        </View>
+
+
       </View>
     )
   }
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F8F9FA',
+  },
+  texto: {
+    fontFamily: 'Times New Roman',
+    marginBottom: 20,
+    fontSize: 18,
+    color: '#333',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+  indicador: {
+    marginVertical: 10,
+  },
+  botonContainer: {
+    marginTop: 30,
+  },
+});
